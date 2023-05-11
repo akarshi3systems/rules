c8b68114351ed77ee3025f10d978634e23007002
akarsh-kumar <akarsh.kumar@i3systems.in>
2023-05-11 17:38:43 +0530

    added rules

--- query.py
+++ query.py
@@ -10,3 +10,5 @@ from powerai_datamd.scripts.rule_engine_helper import setup_mapping_files
 path = os.path.join(settings.BASE_DIR, "configs/rule_engine", "BALIC", "mapping.csv")
 RecordVersion.objects.all().delete()
 setup_mapping_files("BALIC", path)
+
+RuleParameterMaster.objects.filter(external_id=platelet_count).update(reference_string="DataPointValue|dp_config_master__ml_key:ValueTermStandardized|dp_config_master__profile_term__ml_name:Platelet Count|value")
